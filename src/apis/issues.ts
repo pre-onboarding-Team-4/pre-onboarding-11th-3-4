4e516bb53793ce42ad992db0a020cd00b99b43e6
JYROH <ghgt1@naver.com>
2023-07-12 17:00:17 +0900

    feat: 이슈 관련 api 정의 및 dispatch 연동

---
+++ src/apis/issues.ts
@@ -0,0 +1,58 @@
+import { Dispatch } from 'react';
+import axios from 'axios';
+import { IssueListAction } from '../context/IssueListProvider';
+import { IssueDetailAction } from '../context/IssueDetailProvider';
+
+const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_GITHUB_API_ENDPOINT}`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export async function getIssueList(dispatch: Dispatch<IssueListAction>, page: number) {
+  dispatch({ type: 'SET_LOADING' });
+  try {
+    const response = await instance.get(`issues?sort=comments&per_page=10&page=${page}`);
+    dispatch({ type: 'GET_ISSUE_LIST', data: response.data });
+  } catch (error) {
+    Promise.reject(error);
+  }
+}
+
+export async function getIssueDetail(dispatch: Dispatch<IssueDetailAction>, id: number) {
+  dispatch({ type: 'SET_LOADING' });
+  try {
+    const response = await instance.get(`issues/${id}`);
+    dispatch({ type: 'GET_ISSUE_DETAIL', data: response.data });
+  } catch (error) {
+    Promise.reject(error);
+  }
+}
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = process.env.REACT_APP_ACCESS_TOKEN;
+    const newConfig = config;
+    if (accessToken !== undefined) {
+      newConfig.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return newConfig;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const { data } = error.response;
+    if (!data.message) {
+      return Promise.reject(new Error('알 수 없는 에러가 발생했습니다.'));
+    }
+    return Promise.reject(new Error(data.message));
+  },
+);
