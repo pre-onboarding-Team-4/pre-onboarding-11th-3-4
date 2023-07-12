05c7c1f761b915dff2d4a441a57c4ab8c79e8862
Hyeondoonge <migu554@naver.com>
2023-07-13 02:49:52 +0900

    fix: 경로 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import { GlobalStyle } from './GlobalStyle';
-import IssuesContextProvider from './IssuesContextProvider';
+import IssuesContextProvider from './contexts/IssuesContextProvider';
 import IssueContextProvider from './contexts/IssueContextProvider';
 
 function App() {
