45fb4b15b71dd18d0a21259d1c8c9c4298af2cac
김다인 <dbslzhs1218@naver.com>
2023-07-13 20:43:34 +0900

    feat : outlet 설정

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import CommonPage from './pages/CommonPage';
+import IssueListPage from './pages/IssueListPage';
+import IssueDetailPage from './pages/IssueDetailPage';
 
 const route = [
   {
     path: '',
-    element: <div>issue list</div>,
+    element: <CommonPage />,
     errorElement: <div>error</div>,
-  },
-  {
-    path: 'issues/:id',
-    element: <div>issue</div>,
+    children: [
+      { path: '', element: <IssueListPage /> },
+      { path: 'issues/:id', element: <IssueDetailPage /> },
+    ],
   },
 ];
 
