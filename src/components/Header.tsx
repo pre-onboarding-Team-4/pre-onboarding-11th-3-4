1544938220378832e61af9076b6df8bc2c2c29cc
김다인 <dbslzhs1218@naver.com>
2023-07-14 03:59:48 +0900

    feat: 헤더 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-function Header() {
-  return <div>Header</div>;
+function Header({own, repo}: {own:string, repo:string}) {
+  return (
+    <h1>
+      {own} / {repo}
+    </h1>
+    );
 }
 export default Header;
--- src/pages/CommonPage.tsx
+++ src/pages/CommonPage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 
+
 function CommonPage() {
+
   return (
     <div>
-      <Header />
+      <Header own={'facebook'} repo={'react'}/>
 
       <Outlet />
     </div>
