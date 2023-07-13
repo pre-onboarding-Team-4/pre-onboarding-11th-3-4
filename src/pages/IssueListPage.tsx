452edce5538bc7cda944e1ae34dcc7e7edf6a8cf
김다인 <dbslzhs1218@naver.com>
2023-07-13 23:35:04 +0900

    feat: 이슈 목록 보여주기

--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -1,18 +1,35 @@
 import React, { useEffect} from "react";
 import { useIssues } from "../hooks/useIssues";
-
+import { IssueListSchema } from "../types/issuesApi";
 
 function IssueListPage() {
     const { issueList, fetchIssues, fetchMoreIssues, isLoading } = useIssues();
 
-    console.log(issueList);
+    const getFullYmdStr = (d: Date) => {
+        return d.getFullYear() + "년 " + (d.getMonth()+1) + "월 " + d.getDate() + "일 ";
+    }
+
+    //console.log(issueList);
 
     useEffect(()=>{
         fetchIssues();
     }, [])
    
     return(
-        <div></div>
+        issueList.map(function(list, i) {
+            return (
+                <div key={i}>
+                    <div>
+                        <h4>#{list.number} {list.title}</h4>
+                    </div>
+
+                    <div>
+                        <div>작성자:{list.user_id}  작성일:{getFullYmdStr(new Date(list.created_at))}</div> 
+                        <div>코멘트:{list.comments}</div>
+                    </div>
+                </div>
+            )
+        })
     )
 
 }
