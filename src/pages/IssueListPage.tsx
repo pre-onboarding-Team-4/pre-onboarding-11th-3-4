58e18dbc10d88bbe03973cf9c20c60a2e6dee058
김다인 <dbslzhs1218@naver.com>
2023-07-14 08:13:20 +0900

    feat: useRef 추가

--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -1,21 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useCallback, useState } from 'react';
 import { useIssues } from '../hooks/useIssues';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../components/Loading';
 
 function IssueListPage() {
   const navigate = useNavigate();
+  const bottomRef = useRef<HTMLDivElement>(null);
+
   const { issueList, fetchIssues, fetchMoreIssues, isLoading } = useIssues();
-
-  const getFullYmdStr = (d: Date) => {
-    return d.getFullYear() + '년 ' + (d.getMonth() + 1) + '월 ' + d.getDate() + '일 ';
-  };
-
-  //console.log(issueList);
 
   useEffect(() => {
     fetchIssues();
   }, []);
+
+
+  //console.log(issueList);
+
+  const getFullYmdStr = (d: Date) => {
+    return d.getFullYear() + '년 ' + (d.getMonth() + 1) + '월 ' + d.getDate() + '일 ';
+  };
 
   return (
     <div>
