4c61fecbb43f788ce34f6dcb11b09e3781e0d4e7
Hyeondoonge <migu554@naver.com>
2023-07-13 22:25:29 +0900

    feat: intersection observer 동작을 위한 hook 추가

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,27 @@
+import { useRef } from 'react';
+
+export default function useIntersectionObserver(callback: () => void) {
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            callback();
+            console.log('observe');
+          }
+        });
+      },
+      { threshold: 1 },
+    ),
+  );
+
+  const observe = (element: HTMLElement) => {
+    observer.current.observe(element);
+  };
+
+  const unobserve = (element: HTMLElement) => {
+    observer.current.unobserve(element);
+  };
+
+  return { observe, unobserve };
+}
