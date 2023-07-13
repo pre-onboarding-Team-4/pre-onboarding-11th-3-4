c2c7049ffd4f52b0f9fc11a501f7f938fdd4689b
Hyeondoonge <migu554@naver.com>
2023-07-13 22:32:21 +0900

    feat: 이슈 목록 페이지 구현 - 무한 스크롤 구현 - 목록 렌더링

--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useIssues } from '../hooks/useIssues';
+import Loading from '../components/Loading';
+import IssueList from '../components/IssueList';
+import useIntersectionObserver from '../hooks/useIntersectionObserver';
 
-interface IssueListPageProps {}
+export default function IssueListPage() {
+  const owner = process.env.REACT_APP_OWNER;
+  const repo = process.env.REACT_APP_REPO;
 
-export default function IssueListPage(props: IssueListPageProps) {
-  console.log(props);
-  return <div>IssueList</div>;
+  if (!owner || !repo) throw new Error('요구하는 환경 변수가 선언됐는지 확인해주세요');
+
+  const { fetchMoreIssues, fetchIssues, issueList, isLoading } = useIssues();
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchIssues();
+  }, []);
+
+  const target = useRef(null);
+
+  const { observe, unobserve } = useIntersectionObserver(() => {
+    setPage((page) => page + 1);
+  });
+
+  useEffect(() => {
+    if (!target.current) return;
+    observe(target.current);
+    return () => {
+      if (!target.current) return;
+      unobserve(target.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetchMoreIssues();
+  }, [page]);
+
+  return (
+    <div>
+      <IssueList issueList={issueList} />
+      <div style={{ height: 50 }} ref={target} />
+      {isLoading && <Loading />}
+    </div>
+  );
 }
