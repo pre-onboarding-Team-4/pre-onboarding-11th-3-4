3808581a5953e75bf313140ddd52a340eb801b93
Hyeondoonge <migu554@naver.com>
2023-07-13 16:13:06 +0900

    feat: 환경변수 타입 추가 및 적용

--- src/components/CommonLayout.tsx
+++ src/components/CommonLayout.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function CommonLayout() {
+  const owner = process.env.REACT_APP_OWNER;
+  const repo = process.env.REACT_APP_REPO;
+
+  if (!owner || !repo) throw new Error('요구하는 환경 변수가 선언됐는지 확인해주세요');
+
   return (
     <>
       Header
+      {owner}, {repo}
       <Outlet />
     </>
   );
