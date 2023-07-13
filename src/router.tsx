aa6c712bb44eef0985147df099216d57b5a8069a
Hyeondoonge <migu554@naver.com>
2023-07-14 02:56:09 +0900

    feat: Client side 404 에러 핸들링

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import CommonLayout from './components/CommonLayout';
 import IssueDetailPage from './pages/IssueDetailPage';
 import IssueListPage from './pages/IssueListPage';
+import Error from './components/Error';
 
 const route = [
   {
     path: '',
     element: <CommonLayout />,
+    errorElement: <Error message="Not Found Page" />,
     children: [
       {
         path: '',
